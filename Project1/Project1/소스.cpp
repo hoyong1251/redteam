4cfd2e5e40a647d02d587c003f6b0d8f1e9afcd6
hoyong1251 <64102541+hoyong1251@users.noreply.github.com>
2021-04-09 15:30:53 +0900

    Add files via upload

---
+++ Project1/Project1/소스.cpp
@@ -0,0 +1,12 @@
+#include "Income.h"
+
+
+int main()
+{
+	cashData cD;
+	cD.menu(false);
+	//cD.readTextFile("test.txt");
+	//cD.WriteTextFile("test.txt");
+	//cD.keyword_search("*");
+	//cD.yesorno();
+}
